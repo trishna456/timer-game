cec6ffa98f456ea07628c89bb8afcf7b16ad9ad8
Trishna <trishna.patil13@gmail.com>
2024-04-11 19:44:59 -0400

    shifted dialog modal to body using portal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom"
+
 
 const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, onReset}, ref){ //takes 2 arguments now because of forwardRef. 1st is props, 2nd is the ref
 
@@ -16,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
         }
     })
 
-    return(
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2> You Lost! </h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
@@ -29,7 +31,8 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>, 
+        document.getElementById('modal')
     )
 })
 
