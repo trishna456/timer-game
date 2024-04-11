db096ad86db1d051f66f696b7d64166ea867905a
Trishna <trishna.patil13@gmail.com>
2024-04-10 21:36:32 -0400

    using ref for updating name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,29 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
+
 
 export default function Player() {
 
   const [playerName, setPlayerName] = useState('')
-  const [isName, setIsName] = useState(false)
+  //const [isName, setIsName] = useState(false)
+  const enteredName = useRef()
 
-  function handleNameChange(e){
+  /*function handleNameChange(e){
     setIsName(false)
     setPlayerName(e.target.value)
-  }
+  }*/
 
   function handleSetNameClick(){
-    setIsName(true)
+    //setIsName(true)
+    //console.log(enteredName.current)
+    setPlayerName(enteredName.current.value)
+    enteredName.current.value=''
   }
 
   return (
     <section id="player">
-      <h2>Welcome {isName? playerName : 'unknown entity'}</h2>
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" value={playerName} onChange={handleNameChange} />
+        <input ref={enteredName} type="text" />
         <button onClick={handleSetNameClick}>Set Name</button>
       </p>
     </section>
